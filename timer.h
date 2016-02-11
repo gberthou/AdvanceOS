829ea1256cef99ef95e79a78a3d7615d56cf8557
Gautier Berthou <gautier.berthou@insa-lyon.fr>
2016-02-11 18:50:38 +0100

    Changed TimerGetTicks into macro to reduce its footprint

--- timer.h
+++ timer.h
@@ -8,9 +8,14 @@
 #define CLOCK_LCD (CLOCK_FREQ/13620)
 #define CLOCK_TIMER CLOCK_FREQ
 
+#define TMR_BASE 0x20003000
+#define TMR_CS  ((volatile uint32_t*)TMR_BASE)
+#define TMR_CLO (TMR_CS + 1)
+
 void TimerInit(void);
 void TimerCheckIRQ(void);
-uint32_t TimerGetTicks(void);
+
+#define TimerGetTicks() (*TMR_CLO)
 
 #endif
 
